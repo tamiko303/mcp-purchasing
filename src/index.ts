f222dc9401b5e4545623e93be8580268dc649521
Tatsiana Kastrubaiu <tatsiana.kastrubai@artocons.com>
2026-05-19 14:24:02 +0300

    fix import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { Server } from '@modelcontextprotocol/sdk/server/index.js';
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
-import { StreamableHTTPServerTransport } from '@modelcontextprotocol/sdk/server/streamablehttp.js';
+import { StreamableHTTPServerTransport } from '@modelcontextprotocol/sdk/server/streamableHttp.js';
 import {
     CallToolRequestSchema,
     ErrorCode,
